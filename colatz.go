94413c6c769976e60c1ad9c443542b7b86078c08
aenim <mail@mail.com>
2023-03-11 20:09:24 +0300

    working script

---
+++ colatz.go
@@ -0,0 +1,36 @@
+package main
+import (
+	"fmt"
+)
+func main(){
+	collatzalgtorythm(9903)
+}
+
+func collatzalgtorythm(x int64){
+
+	count := 1
+	inputvalue := x
+	
+	for {	
+		if x!=1 {
+			if x%2 != 0 {
+				z := 3*x+1
+				fmt.Print(z, " ")
+				count++
+				x = z
+
+			} else {
+				z := x/2
+				fmt.Print(z, " ")	
+				count++
+				x = z
+				
+			}
+			
+		} else {
+			fmt.Println("")
+			fmt.Println("For number", inputvalue, "was done:", count, "steps.")
+			break
+		}
+	}
+}
